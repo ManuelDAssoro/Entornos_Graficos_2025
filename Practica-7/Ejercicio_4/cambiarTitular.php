862e76e73ec666ef52e2efc90e72a35af953d122
manuel <73363119+ManuelDAssoro@users.noreply.github.com>
2025-06-10 15:05:42 -0300

    add newspaper page with improved form accessibility and usability

--- Practica-7/Ejercicio_4/cambiarTitular.php
+++ Practica-7/Ejercicio_4/cambiarTitular.php
@@ -2,6 +2,28 @@
     if(isset($_POST['titular'])){
         $noticia = $_POST['titular'];
     }
-    echo "Noticia ";
-    echo $noticia;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noticia</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>El Diario</h1>
+    </header>
+    <main>
+        <h1>
+            <?php
+            echo "Noticia " ;
+            echo $noticia;
+            ?>
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nisi id libero tincidunt tincidunt. Nullam vehicula, justo a tincidunt fermentum, eros lorem tincidunt libero, nec tincidunt ligula lorem non nisi.</p>
+    </main>
+</body>
+</html>
--- Practica-7/Ejercicio_4/index.php
+++ Practica-7/Ejercicio_4/index.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 4</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
     <form action="cambiarTitular.php" method="post">
         <p>Seleccione el tipo de titular:</p>
         <br>
-        <input type="radio" name="titular" value="Politica">
+        <input type="radio" id="politica" name="titular" value="Politica">
         <label for="politica">Noticia Politica</label>
         <br>
-        <input type="radio" name="titular" value="Economica">
-        <label for="politica">Noticia Economica</label>
+        <input type="radio" id="economica" name="titular" value="Economica">
+        <label for="economica">Noticia Economica</label>
         <br>
-        <input type="radio" name="titular" value="Deportiva">
-        <label for="politica">Noticia Deportiva</label>
+        <input type="radio" id="deportiva" name="titular" value="Deportiva">
+        <label for="deportiva">Noticia Deportiva</label>
         <br>
         <input type="submit" value="Enviar">
     </form>
